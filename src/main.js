f40aba0598e53b2bc05dee493de592748e16761c
unknown <hoangvinh13159200@gmail.com>
2020-08-27 21:03:15 +0700

    first commit

---
+++ src/main.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const morgan = require('morgan');
+const app = express();
+const port = 3000;
+
+app.get('/',(req, res)=>{
+    res.send('home');
+})
+app.listen(port, ()=>{
+    console.log(`Example app listening at http://localhost:${port}`)
+})

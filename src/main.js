6782b5539e9739778f40c7a263aaf8eed2c28db8
unknown <hoangvinh13159200@gmail.com>
2020-08-28 11:16:22 +0700

    second

--- src/main.js
+++ src/main.js
@@ -1,10 +1,16 @@
 const express = require('express');
+const path = require('path');
 const morgan = require('morgan');
+const handlebars = require('express-handlebars')
 const app = express();
 const port = 3000;
 
+app.set('views', path.join(__dirname, './resources/views')); //cài đặt địa chỉ đường dẫn cho thư mục views
+
+app.engine('.hbs', handlebars({extname: '.hbs'}));
+app.set('view engine', '.hbs');
 app.get('/',(req, res)=>{
-    res.send('home');
+    res.render('home');
 })
 app.listen(port, ()=>{
     console.log(`Example app listening at http://localhost:${port}`)
